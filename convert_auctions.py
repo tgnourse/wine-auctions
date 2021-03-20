4f8fd459b5fc27f7d8273802d6c3c8c8f7582033
Thomas Nourse <tgnourse@gmail.com>
2021-03-19 21:30:01 -0700

    Update parsing for new website code.

--- convert_auctions.py
+++ convert_auctions.py
@@ -10,15 +10,25 @@ from dateutil.parser import parse
 
 class AuctionHTMLParser(HTMLParser):
 
-    RECORD = 'result clearfix'
+    RECORD = 'tf-product clearfix'
 
-    SECTION_IMAGE = 'auctionProductImg'
-    SECTION_DESCRIPTION = 'auctionResult-desc'
-    SECTION_INFO = 'result-info'
-    SECTION_RATING = 'rating'
+    # <div class="tf-pill">RP: 98</div>
+    SECTION_RATING = 'tf-pill'
 
-    RATING_RATING = 'rating global-serif'
-    RATING_RATER = 'rater'
+    # <span class="global-pop-color" style=font-weight:bold>$351.00</span>
+    SECTION_PRICE = 'global-pop-color'
+
+    # <div class="tf-items">2012 Alban "Lorraine" Edna Valley Syrah (qty: 3)</div>
+    SECTION_ITEMS = 'tf-items'
+
+    # <strong class="tf-auction-end-time past-due">End Date: Mar 17 2021 11:00AM PT</strong>
+    SECTION_DATE = 'tf-auction-end-time past-due'
+
+    # <div class="tf-product-description">Bid on this 3-bottle lot of 2012 ... </div>
+    SECTION_DESCRIPTION = 'tf-product-description'
+
+    # data-app-insights-track-search-click=AuctionItemClick
+    HREF_IMAGE = 'AuctionItemClick'
 
     def __init__(self):
         # Can't call super() because this derives from an old style class.
@@ -41,61 +51,57 @@ class AuctionHTMLParser(HTMLParser):
                 # Now we're in the record so create a new one.
                 self.current_record = {'ratings': {}}
                 self.records.append(self.current_record)
-            elif attrs_dict.get('class') in [self.SECTION_IMAGE, self.SECTION_DESCRIPTION, self.SECTION_INFO]:
+            elif attrs_dict.get('class') in [self.SECTION_RATING, self.SECTION_ITEMS, self.SECTION_DESCRIPTION]:
                 # We've entered one of the data sections in the record.
                 self.current_section = attrs_dict.get('class')
-
-        if tag == 'span':
-            if attrs_dict.get('class') == self.SECTION_RATING:
-                self.current_section = self.SECTION_RATING
-
-        # Do section specific parsing.
-        if self.current_section == self.SECTION_DESCRIPTION:
-            if tag == 'a':
-                self.parse_title(attrs_dict.get('title'))
-                self.current_record['link'] = attrs_dict.get('href')
-                sku_match = re.search(r'sku=(\d+)', attrs_dict.get('href'))
-                if sku_match:
-                    self.current_record['sku'] = int(sku_match.group(1))
-
-        if self.current_section == self.SECTION_RATING:
-            if tag == 'span':
-                if attrs_dict.get('class') in [self.RATING_RATER, self.RATING_RATING]:
-                    self.current_rating_section = attrs_dict.get('class')
+        elif tag == 'span' and attrs_dict.get('class') in [self.SECTION_PRICE]:
+            self.current_section = attrs_dict.get('class')
+        elif tag == 'strong' and attrs_dict.get('class') in [self.SECTION_DATE]:
+            self.current_section = attrs_dict.get('class')
+        elif tag == 'a' and attrs_dict.get('data-app-insights-track-search-click') in [self.HREF_IMAGE]:
+            self.current_record['sku'] = attrs_dict.get('data-app-insights-track-search-doc-id')
+            if attrs_dict.get('href'):
+                self.current_record['link'] = 'https://www.klwines.com/' + attrs_dict.get('href').split('&')[0]
+            if attrs_dict.get('title'):
+                self.current_record['title'] = attrs_dict.get('title').strip()
 
     def handle_data(self, data):
         data = data.strip()
-        if self.current_section == self.SECTION_DESCRIPTION:
-            # Find the quantity in data for this section.
+
+        if self.current_section == self.SECTION_RATING:
+            match = re.search(r'([\w\d]+): ([\w\d]+)', data)
+            if match:
+                self.current_record['ratings'][match.group(1)] = match.group(2)
+
+        if self.current_section == self.SECTION_ITEMS:
+            self.parse_title(data)
             match = re.search(r'qty: ([\d]+)', data)
             if match:
                 self.current_record['quantity'] = int(match.group(1))
-                return
-            # Find and parse the end date which occurs in this section.
+
+        if self.current_section == self.SECTION_DATE:
             match = re.search(r'End Date: ([\w\d: ]+)', data)
             if match:
                 self.current_record['end_date'] = parse(match.group(1))
 
-        if self.current_section == self.SECTION_INFO:
-            # Get the current bid price.
-            if data != '' and self.current_tag == 'strong':
-                self.current_record['current_bid'] = int(Decimal(re.sub(r'[^\d.]', '', data)) * 100)
+        if self.current_section == self.SECTION_PRICE:
+            self.current_record['current_bid'] = int(Decimal(re.sub(r'[^\d.]', '', data)) * 100)
 
-        if self.current_section == self.SECTION_RATING:
-            if self.current_rating_section == self.RATING_RATER:
-                self.current_rater = data
-                self.current_rating_section = None
-            if self.current_rating_section == self.RATING_RATING and self.current_rater:
-                self.current_record['ratings'][self.current_rater] = data
-                self.current_rating_section = None
-                self.current_rater = None
+        if self.current_section == self.SECTION_DESCRIPTION:
+            self.current_record['description'] = self.current_record.get('description', '') + data
 
     def handle_endtag(self, tag):
         self.current_tag = None
+        self.current_section = None
 
     def parse_title(self, title):
         # Save the full title.
-        self.current_record['title'] = title
+        self.current_record['title_quantity'] = title
+
+        # Pull out the name.
+        name_match = re.search(r'([\d-]*)(.*)(\(.*\))', title)
+        if name_match:
+            self.current_record['name'] = name_match.group(2).strip()
 
         # Separate the vintage.
         vintage_match = re.search(r'(\d\d\d\d)', title)
